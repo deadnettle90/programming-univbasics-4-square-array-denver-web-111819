8c704b97f0799882e836d38778eaa3b574dd0e4e
Danielle Matton <danimatton@gmail.com>
2019-10-28 02:23:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(numbers)
   new_array = []
   i = 0
   while i < numbers.length do
-    new_array[i] = numbers[i] ** 2
+    new_array.push(numbers[i] ** 2)
     i += 1
   end
   puts new_array
